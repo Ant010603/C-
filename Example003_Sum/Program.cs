1198c53769f81933a1d5180822ac810021cfbf13
Anton <Lisavonanton03@gmail.com>
2022-10-12 13:24:01 +0600

    Example 005 if

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,4 +1,5 @@
-﻿int numberA = 3;
-int numberB = 5;
+﻿int numberA = new Random().Next(1, 10); // 0 1 2 3 4 ... 9
+int numberB = new Random().Next(1, 10);
+int result  = numberA + numberB;
 Console.WriteLine(numberA + numberB);
 
